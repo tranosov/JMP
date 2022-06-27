899a0613df4651177bd4e671bc8c7cdce4090210
TerezaR <tranosova@umich.edu>
2022-06-27 16:38:51 -0500

    Update labors.m

--- labors.m
+++ labors.m
@@ -170,7 +170,7 @@ for i=1:I
                     fprintf('Warning')                    
                     options0 = optimoptions('fsolve','MaxIter',5000,'MaxFunctionEvaluations',5000,...
                             'FunctionTolerance',TOL,'Display','off','Algorithm','trust-region'); %,'StepTolerance', STEPTOL);
-                    [output1,~,EXITFLAG]=fsolve(fn,[mu00,inputs0(th,tw,jh,jw,i,1,2:3)*1.1],options0);
+                    [output1,~,EXITFLAG]=fsolve(fn,[mu00,reshape(inputs0(th,tw,jh,jw,i,1,2:3),1,2)*1.1],options0);
                     if ~isreal(output1) | output1(1)<=0 | output1(2)<=0 | lsh(output1(1),output1(2)/100,output1(3)/100,dh,0,lambda) <=0 | ((EXITFLAG~=1) && (EXITFLAG~=2)&& (EXITFLAG~=3)&& (EXITFLAG~=4))
                         fprintf('Warning2')
                         WARNINGS=WARNINGS+1;
@@ -201,7 +201,7 @@ for i=1:I
                     fprintf('Warning')
                     options0 = optimoptions('fsolve','MaxIter',5000,'MaxFunctionEvaluations',5000,...
                             'FunctionTolerance',TOL,'Display','off','Algorithm','trust-region','StepTolerance', STEPTOL);
-                    [output2,~,EXITFLAG]=fsolve(fn,[mu000,inputs0(th,tw,jh,jw,i,2,2:3)*1.1],options0);
+                    [output2,~,EXITFLAG]=fsolve(fn,[mu000,reshape(inputs0(th,tw,jh,jw,i,2,2:3),1,2)*1.1],options0);
                     if ~isreal(output2) | output2(1)<=0 | output2(2)<=0 | lsw(output2(1),output2(2)/100,output2(3)/100,0,dw,lambda) <=0 | ((EXITFLAG~=1) && (EXITFLAG~=2)&& (EXITFLAG~=3)&& (EXITFLAG~=4))
                         fprintf('Warning2')
                         WARNINGS=WARNINGS+1;
@@ -234,7 +234,7 @@ for i=1:I
                     fprintf('Warning')
                     options0 = optimoptions('fsolve','MaxIter',5000,'MaxFunctionEvaluations',5000,...
                             'FunctionTolerance',TOL,'Display','off','Algorithm','trust-region','StepTolerance', STEPTOL);
-                    [output3,~,EXITFLAG]=fsolve(fn,[mu0,inputs0(th,tw,jh,jw,i,3,2:3)*1.1],options0);
+                    [output3,~,EXITFLAG]=fsolve(fn,[mu0,reshape(inputs0(th,tw,jh,jw,i,3,2:3),1,2)*1.1],options0);
                     if ~isreal(output3) | output3(1)<=0 | output3(2)<=0 |  lsh(output3(1),output3(2)/100,output3(3)/100,dh,dw,lambda) <=0  | lsw(output3(1),output3(2)/100,output3(3)/100,dh,dw,lambda) <=0 | ((EXITFLAG~=1) && (EXITFLAG~=2)&& (EXITFLAG~=3)&& (EXITFLAG~=4))
                         fprintf('Warning2')
                         WARNINGS=WARNINGS+1;
