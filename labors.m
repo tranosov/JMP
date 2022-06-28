cccd32e4a790cf79e0e7fa6c009ce676b3746b48
TerezaR <ranosova1@gmail.com>
2022-06-27 19:01:05 -0500

    Update labors.m

--- labors.m
+++ labors.m
@@ -243,7 +243,7 @@ for i=1:I
                     end
                     options0 = optimoptions('fsolve','MaxIter',5000,'MaxFunctionEvaluations',5000,...
                             'FunctionTolerance',TOL,'Display','off','Algorithm','trust-region','StepTolerance', STEPTOL);
-                    [output3,~,EXITFLAG]=fsolve(fn,reshape(inputs0(th,tw,jh,jw,i,3,:)*1.1,options0);
+                    [output3,~,EXITFLAG]=fsolve(fn,inputs0(th,tw,jh,jw,i,3,:)*1.1,options0);
                     if ~isreal(output3) | output3(1)<=0 | output3(2)<=0 |  lsh(output3(1),output3(2)/100,output3(3)/100,dh,dw,lambda) <=0  | lsw(output3(1),output3(2)/100,output3(3)/100,dh,dw,lambda) <=0 | ((EXITFLAG~=1) && (EXITFLAG~=2)&& (EXITFLAG~=3)&& (EXITFLAG~=4))
                         
                         [output3,~,EXITFLAG]=fsolve(fn,[mu0,reshape(inputs0(th,tw,jh,jw,i,3,2:3),1,2)*1.1],options0);
