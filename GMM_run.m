87c052bbfa311c173d0e8e274dcce4070c0ddb98
TerezaR <ranosova1@gmail.com>
2022-07-05 16:13:32 -0500

    Update GMM_run.m

--- GMM_run.m
+++ GMM_run.m
@@ -10,6 +10,16 @@ clear global;
 rng(357)
 fprintf('Running GMM estimation. CHECK NEW');
 
+
+filename = "./estimation/progressmoment.txt";
+io = fopen(filename,'a');
+fprintf(io,"Rerun routine. \n");
+filename = "./estimation/progress.txt";
+io = fopen(filename,'a');
+fprintf(io,"Rerun routine. \n");
+
+
+%%
 forparams =readtable('./input/PREP.xlsx','Sheet','PARS','ReadVariableNames', true,'ReadRowNames',true);
 formom =readtable('./input/PREP.xlsx','Sheet','MOMS','ReadVariableNames', true,'ReadRowNames',true);
 forw =readtable('./input/PREP.xlsx','Sheet','W','ReadVariableNames', true,'ReadRowNames',true);
@@ -28,12 +38,14 @@ RESC=10^3;
 global VERBOSE
 VERBOSE=0;
 
+
 %fopt=10^(-6); % no idea
 x0=table2array(paramsest);
 LB=table2array(forparams(paramsest.Properties.RowNames,'min'));
 UB=table2array(forparams(paramsest.Properties.RowNames,'max'));
 GG=GMM(x0,pars,momentest,W,momentall,paramsall);
 GG
+%GG=6;
 
 
 ObjectiveFunction=@(x)GMM(x,pars,momentest,W,momentall,paramsall); % pars has the list!
@@ -41,8 +53,9 @@ rng(357);
 options = optimoptions(@simulannealbnd,'MaxFunctionEvaluations',10000,'Display','diagnose');
 % increase temp to have more acceptence
 options.InitialTemperature = 100*max(GG,1); %I think should be in scale of objective function (or like jacobian - how params affect obj function)
-temperature = @(optimValues,options) options.InitialTemperature.*(0.99.^optimValues.k); % slow down?
-options.TemperatureFcn=temperature;
+%temperature = @(optimValues,options) options.InitialTemperature.*(0.99.^optimValues.k); % slow down?
+%options.TemperatureFcn=temperature;
+%options.ReannealInterval=50;
 
 [x,fval,exitFlag,output] = simulannealbnd(ObjectiveFunction,x0,LB,UB,options)
 output
@@ -55,3 +68,4 @@ output
 %temperature = @(optimValues,options) options.InitialTemperature.*(0.99^optimValues.k)
 %options.TemperatureFcn=@temperature
 
+saupdates
