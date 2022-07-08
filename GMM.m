25b7234943e472fda2a54c72affb09452e197a5f
TerezaR <ranosova1@gmail.com>
2022-07-07 23:25:16 -0500

    rescaling + ignore another moment
    + cathing errors in regressions

--- GMM.m
+++ GMM.m
@@ -2,19 +2,18 @@ function [G]=GMM(pars_,pars,momentest,W,momentall,params)
 % set model
 % solve model
 % create moments to compare to daya
-global VERBOSE
+global VERBOSE %DOWN
 
-[moments_,time, EXITFLAG]=GMMmoments(pars_,pars,momentest,W,momentall,params);
+[moments_,time, EXITFLAG,params_]=GMMmoments(pars_,pars,momentest,W,momentall,params);
 
-UP=10^(-6); % scale down. it is reallyhigh.
 
 if EXITFLAG==999
-    G=10^(23)*UP;
+    G=10^(23);
 else
-    G=(moments_-table2array(momentest))'*W*(moments_-table2array(momentest))*UP;
+    G=(moments_-table2array(momentest))'*W*(moments_-table2array(momentest));
 end
 momnames=momentest.Properties.RowNames;
-pnames=params.Properties.RowNames;
+pnames=params_.Properties.RowNames;
 
 
 
@@ -36,12 +35,22 @@ fprintf(io,"GMM function value =   %16.8f\n",G);
 fprintf(io," \n");
 fprintf(io,"time =   %16.8f\n",time);
 
+
 if EXITFLAG~=999
-    fprintf(io," data and simulated moments\n");
+    fprintf(io," data and simulated moments, and dif, absdif, weighted dif \n");
+    weightedm=((moments_-table2array(momentest))'*W).*(moments_-table2array(momentest))';
     for jj=1:size(moments_,1)
         fprintf(io,"%s",char(momnames(jj,1)));
         fprintf(io,"%16.8f",table2array(momentest(jj,1)) );  
         fprintf(io,"%16.8f\n",moments_(jj,1)) ; 
+    end
+    fprintf(io," data and simulated moments: and dif and weighted dif \n");
+    weightedm=((moments_-table2array(momentest))'*W).*(moments_-table2array(momentest))';
+    for jj=1:size(moments_,1)
+        fprintf(io,"%s",char(momnames(jj,1)));
+        fprintf(io,"%16.8f",table2array(momentest(jj,1)) -moments_(jj,1)) ; 
+        %fprintf(io,"%16.8f ",abs(table2array(momentest(jj,1)) -moments_(jj,1))) ; 
+        fprintf(io,"%16.8f\n",weightedm(1,jj)) ; 
     end
 else
     fprintf(io," data and simulated moments skipped.\n");
@@ -52,7 +61,7 @@ fprintf(io," \n");
 fprintf(io," parameter value\n");
 for jj =1:size(params,1)
     fprintf(io,"%s",char(pnames(jj,1)));
-    fprintf(io,"%16.8f\n",table2array(params(jj,1)));
+    fprintf(io,"%16.8f\n",table2array(params_(jj,1)));
 end
 fclose(io);
 
