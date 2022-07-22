5304986963abe8c4c96039e12a86a5183abcc5a2
TerezaR <ranosova1@gmail.com>
2022-07-22 08:23:52 -0500

    checks and prints

--- solvep.m
+++ solvep.m
@@ -99,6 +99,14 @@ end
                 [output,FVAL,EXITFLAG,OUTPUT]= fsolve(F,lp0*0.9,options);
 
             end
+            if (EXITFLAG~=1) && (EXITFLAG~=2) && (EXITFLAG~=3) && (EXITFLAG~=4)              
+                WARNINGS=WARNINGS+1;
+                fprintf('Solvep failed many times.')
+                FVAL=FVAL
+                OUTPUT
+                EXITFLAG=999;
+            end
+            
             
             %{
             if (EXITFLAG~=1) && (EXITFLAG~=2) && (EXITFLAG~=3) && (EXITFLAG~=4)
@@ -112,11 +120,7 @@ end
                 %fprintf('Trying again again again');
                 [output,FVAL,EXITFLAG,OUTPUT]= fsolve(F,ones(size(lp0)),options);
 
-            if (EXITFLAG~=1) && (EXITFLAG~=2) && (EXITFLAG~=3) && (EXITFLAG~=4)              
-                WARNINGS=WARNINGS+1;
-                fprintf('Solvep failed many times.')
-                EXITFLAG=999;
-            end
+            
 
             end
 %}
