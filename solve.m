74e53165bd5bb524e7c6a62f76a6f9cddde069d5
TerezaR <ranosova1@gmail.com>
2022-07-21 21:31:25 -0500

    Update solve.m

--- solve.m
+++ solve.m
@@ -68,9 +68,17 @@ if norm(Fm(x0(end)))^2 >tol
         if Fm(x0_(1))*Fm(x0_(2))<0
             out=fzero(Fm,x0_,optionsz);
             if Fm(out)<-0.01
+                try
                 out=fzero(Fm,[out,x0_(2)],optionsz);
+                catch
+                    fprintf('Cant match Fm precisely');
+                end
             elseif Fm(out)>0.01
+                try
                 out=fzero(Fm,[x0_(1),out],optionsz);
+                catch
+                    fprintf('Cant match Fm precisely');
+                end
             end
         else
             fprintf('unexpected behavior at solving for lambda in solve')
