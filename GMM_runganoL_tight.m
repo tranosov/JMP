65e10d1acdc9bd80de639e0d09964c6b21b3f8c8
tranosov <ranosova1@gmail.com>
2023-06-27 15:40:32 -0400

    Update GMM_runganoL_tight.m

--- GMM_runganoL_tight.m
+++ GMM_runganoL_tight.m
@@ -26,8 +26,8 @@ fclose(io);
 
 %%
 forparams =readtable('./input/PREP_tight.xlsx','Sheet','PARS','ReadVariableNames', true,'ReadRowNames',true);
-formom =readtable('./input/PREP.xlsx','Sheet','MOMS','ReadVariableNames', true,'ReadRowNames',true);
-forw =readtable('./input/PREP.xlsx','Sheet','W','ReadVariableNames', true,'ReadRowNames',true);
+formom =readtable('./input/PREP_nolfp.xlsx','Sheet','MOMS','ReadVariableNames', true,'ReadRowNames',true);
+forw =readtable('./input/PREP_NEW.xlsx','Sheet','W','ReadVariableNames', true,'ReadRowNames',true);
 
 fprintf('Inputs loaded.\n');
 
